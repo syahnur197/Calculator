3f3540f4bf180fa3051a8332a645d1ac6f2a3abc
Syahnur <syahnurnizam197@Gmail.com>
2019-08-18 01:48:20 +0800

    we all commit to love

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,78 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="jumbotron">
+      <h1>
+        <span class="fa fa-calculator"></span>
+        Calculator
+      </h1>
     </div>
-    <router-view/>
+    <div class="container">
+      <p>Hello! Welcome to Calculator. Use this website to calculate your volume!</p>
+      <label>Select Your Account</label>
+      <br>
+      <input type="radio" name="standard" value="0" id="" v-model="account"> Standard (100,000 unit per lot) <br>
+      <input type="radio" name="micro" id="" value="1" v-model="account"> Mini (10,000 unit per lot) <br>
+      <input type="radio" name="micro" id="" value="2" v-model="account"> Micro (1,000 unit per lot)
+
+      <br>
+      <label for="">Dollar Risk: </label>
+      <input type="number" name="dollar" id="dollar" v-model="dollar" class="form-control">
+
+      <br>
+      <label for="">Pips Risk: </label>
+      <input type="number" name="pips" id="pips" v-model="pips" class="form-control">
+
+      <br>
+      <label for="">Volume: {{volume}}</label>
+
+    </div>
+    <footer class="footer bg-secondary py-4 fixed-bottom">
+      <div class="container">
+        <span class="text-light">Syahnur Nizam &copy;
+          {{yearNow}}
+        </span>
+      </div>
+    </footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      dollar: 5,
+      pips: 50,
+      account: "0"
+    }
+  },
+
+  computed: {
+    volume() {
+      if(this.account === "0") {
+        return (this.dollar/(10*this.pips)).toFixed(2)
+      } else if (this.account === "1") {
+        return (this.dollar/(this.pips)).toFixed(2)
+      } else {
+        return (this.dollar/(0.1*this.pips)).toFixed(2)
+      }
+    },
+
+    yearNow() {
+      return (new Date()).getFullYear();
+    }
+  },
+
+  methods: {
+    activate(index) {
+      this.buttons[0] = false;
+      this.buttons[1] = false;
+      this.buttons[index] = true;
+      // console.log(this.buttons[index])
     }
   }
 }
+</script>
+
+
+<style lang="scss">
 </style>
